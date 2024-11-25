f4ccf9819466e3e8d35995b6347fb2bc33a4838f
Anastasiia Chernysheva <asay31@gmail.com>
2024-11-25 22:57:37 +0400

    Main class updated with java.awt example

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,30 +1,17 @@
 package ru.courses.main;
 
-import ru.courses.geometry.*;
-import ru.courses.math.*;
-import ru.courses.birds.*;
-
-import java.util.List;
+import ru.courses.geometry.Point; // Импортируем нашу точку
 
 public class Main {
     public static void main(String[] args) {
-        // Пример работы с геометрическими фигурами
-        Figure circle = new Circle(new Point(0, 0), 5);
-        Figure square = new Square(new Point(0, 0), 4);
-        Figure rectangle = new Rectangle(new Point(0, 0), 3, 6);
+        // Экземпляр нашей точки
+        Point customPoint = new Point(3, 4);
 
-        List<Figure> figures = List.of(circle, square, rectangle);
-        double totalArea = FigureUtils.sumAllAreas(figures);
+        // Экземпляр стандартной точки из java.awt, используя полное имя класса
+        java.awt.Point awtPoint = new java.awt.Point(5, 6);
 
-        System.out.println("=== Общая площадь фигур ===");
-        System.out.println("Total Area: " + totalArea);
-
-        // Работа с дробями
-        Fraction fraction1 = new Fraction(1, 3);
-        Fraction fraction2 = new Fraction(2, 5);
-
-        System.out.println("\n=== Работа с дробями ===");
-        System.out.println("Fraction1: " + fraction1);
-        System.out.println("Fraction2: " + fraction2);
+        // Вывод значений точек
+        System.out.println("Custom Point from ru.courses.geometry: " + customPoint);
+        System.out.println("AWT Point from java.awt: " + awtPoint);
     }
 }
